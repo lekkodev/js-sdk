0df20795ff6d0a7f580f670ae615073b5cddc91f
David Kang <david@lekko.com>
2024-02-14 17:35:18 -0800

    Add localPath option to client initialization

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,27 @@ import { type Client } from "./types/client"
 
 interface APIOptions {
   apiKey: string
-  hostname?: string
   repositoryOwner: string
   repositoryName: string
+  /**
+   * For local development. You can pass the URL to an alternative Lekko config
+   * server (e.g. using @lekko/node-server-sdk).
+   */
+  hostname?: string
+  /**
+   * For local development to use with a Lekko config server (e.g. using
+   * @lekko/node-server-sdk) which is set to use a local repository.
+   * The target server should use this path to read from a config repository
+   * on local disk. If localPath is omitted, a default location will be used.
+   */
+  localPath?: string
 }
 
 function initAPIClient(options: APIOptions): Client {
   const transport = new ClientTransportBuilder({
     hostname: options.hostname ?? "https://app.lekko.com/api/",
     apiKey: options.apiKey,
+    localPath: options.localPath,
   }).build()
   return new TransportClient(
     options.repositoryOwner,
