46ce8f5733fc22c9a6da17e33220ba0ae9167c32
David Kang <david@lekko.com>
2024-02-16 11:14:45 -0800

    Add localPath option to client initialization (#5)

--- src/transport-builder.ts
+++ src/transport-builder.ts
@@ -1,12 +1,19 @@
 import { createGrpcWebTransport } from "@bufbuild/connect-web"
 
-import { type Transport } from "@bufbuild/connect"
+import { type Transport, type Interceptor } from "@bufbuild/connect"
 
-const APIKEY_INTERCEPTOR =
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (apiKey?: string) => (next: any) => async (req: any) => {
+const APIKEY_INTERCEPTOR: (apiKey?: string) => Interceptor =
+  (apiKey?: string) => (next) => async (req) => {
     if (apiKey !== undefined) {
       req.header.set("apikey", apiKey)
+    }
+    return next(req)
+  }
+
+const LOCAL_PATH_INTERCEPTOR: (path?: string) => Interceptor =
+  (path?: string) => (next) => async (req) => {
+    if (path !== undefined) {
+      req.header.set("localpath", path)
     }
     return next(req)
   }
@@ -14,10 +21,20 @@ const APIKEY_INTERCEPTOR =
 export class ClientTransportBuilder {
   hostname: string
   apiKey?: string
+  localPath?: string
 
-  constructor({ hostname, apiKey }: { hostname: string; apiKey?: string }) {
+  constructor({
+    hostname,
+    apiKey,
+    localPath,
+  }: {
+    hostname: string
+    apiKey?: string
+    localPath?: string
+  }) {
     this.hostname = hostname
     this.apiKey = apiKey
+    this.localPath = localPath
   }
 
   build(): Transport {
@@ -26,7 +43,10 @@ export class ClientTransportBuilder {
     }
     return createGrpcWebTransport({
       baseUrl: this.hostname,
-      interceptors: [APIKEY_INTERCEPTOR(this.apiKey)],
+      interceptors: [
+        APIKEY_INTERCEPTOR(this.apiKey),
+        LOCAL_PATH_INTERCEPTOR(this.localPath),
+      ],
     })
   }
 }
