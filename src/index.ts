0ab1858ac52cdc14632657b26f76265c6d87b7ce
Bill Zdon <williamzdon@gmail.com>
2024-03-25 17:37:42 -0300

    fix default hostname

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ async function initCachedAPIClient(
   options: BackendOptions,
 ): Promise<SyncClient> {
   const transport = new ClientTransportBuilder({
-    hostname: options.hostname ?? "https://prod.api.lekko.dev",
+    hostname: options.hostname ?? "https://app.lekko.com/api",
     apiKey: options.apiKey,
   }).build()
   const client = new Backend(
